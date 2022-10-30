f900be2c35f56c9d0d9ea6c7ec0e0e0e60b0e778
ThomasRgl <thomasroglin@free.fr>
2022-10-30 17:49:10 +0100

    adding UNROLL8 and UNROLL16

--- dgemm/kernels.c
+++ dgemm/kernels.c
@@ -61,6 +61,58 @@ void dgemm_unroll4(f64 *restrict a, f64 *restrict b, f64 *restrict c, u64 n) {
     }
 }
 
+//
+void dgemm_unroll8(f64 *restrict a, f64 *restrict b, f64 *restrict c, u64 n) {
+#define UNROLL8 8
+
+    for (u64 i = 0; i < n; i++) {
+        for (u64 k = 0; k < n; k++) {
+            const f64 _a_ = a[i * n + k];
+
+            for (u64 j = 0; j < n; j += UNROLL8) {
+                c[i * n + j] += _a_ * b[k * n + j];
+                c[i * n + j + 1] += _a_ * b[k * n + j + 1];
+                c[i * n + j + 2] += _a_ * b[k * n + j + 2];
+                c[i * n + j + 3] += _a_ * b[k * n + j + 3];
+                c[i * n + j + 4] += _a_ * b[k * n + j + 4];
+                c[i * n + j + 5] += _a_ * b[k * n + j + 5];
+                c[i * n + j + 6] += _a_ * b[k * n + j + 6];
+                c[i * n + j + 7] += _a_ * b[k * n + j + 7];
+            }
+        }
+    }
+}
+
+//
+void dgemm_unroll16(f64 *restrict a, f64 *restrict b, f64 *restrict c, u64 n) {
+#define UNROLL16 16
+
+    for (u64 i = 0; i < n; i++) {
+        for (u64 k = 0; k < n; k++) {
+            const f64 _a_ = a[i * n + k];
+
+            for (u64 j = 0; j < n; j += UNROLL16) {
+                c[i * n + j] += _a_ * b[k * n + j];
+                c[i * n + j + 1] += _a_ * b[k * n + j + 1];
+                c[i * n + j + 2] += _a_ * b[k * n + j + 2];
+                c[i * n + j + 3] += _a_ * b[k * n + j + 3];
+                c[i * n + j + 4] += _a_ * b[k * n + j + 4];
+                c[i * n + j + 5] += _a_ * b[k * n + j + 5];
+                c[i * n + j + 6] += _a_ * b[k * n + j + 6];
+                c[i * n + j + 7] += _a_ * b[k * n + j + 7];
+                c[i * n + j + 8] += _a_ * b[k * n + j + 8];
+                c[i * n + j + 9] += _a_ * b[k * n + j + 9];
+                c[i * n + j + 10] += _a_ * b[k * n + j + 10];
+                c[i * n + j + 11] += _a_ * b[k * n + j + 11];
+                c[i * n + j + 12] += _a_ * b[k * n + j + 12];
+                c[i * n + j + 13] += _a_ * b[k * n + j + 13];
+                c[i * n + j + 14] += _a_ * b[k * n + j + 14];
+                c[i * n + j + 15] += _a_ * b[k * n + j + 15];
+            }
+        }
+    }
+}
+
 
 void dgemm_cblas(f64 *restrict a, f64 *restrict b, f64 *restrict c, u64 n) {
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, a, n,
--- dgemm/main.c
+++ dgemm/main.c
@@ -49,6 +49,8 @@ int main(int argc, char **argv) {
     run_benchmark("IKJ", dgemm_ikj, n, r);
     run_benchmark("IEX", dgemm_iex, n, r);
     run_benchmark("UNROLL4", dgemm_unroll4, n, r);
+    run_benchmark("UNROLL8", dgemm_unroll8, n, r);
+    run_benchmark("UNROLL16", dgemm_unroll16, n, r);
     run_benchmark("CBLAS", dgemm_cblas, n, r);
 
     //
